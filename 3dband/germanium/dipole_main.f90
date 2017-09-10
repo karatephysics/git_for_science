5dc25891c6364fcb3cd968a228c8035858d2ad7e
STetsuya <Tetsu@TS-MacBookAir.local>
2017-09-10 14:14:15 +0900

    message

--- 3dband/germanium/dipole_main.f90
+++ 3dband/germanium/dipole_main.f90
@@ -2,9 +2,8 @@ program bandgermanium
 
 implicit none
 
-real(8)::e_ini,e_fin,nn,sa,e0,e1,e2,e3
+real(8)::sa,laser_ev
 real(8)::kx,ky,kz
-real(8)::moji
 integer::i,j,ss,nkx,nky,nkz
 integer,parameter::n=30
 integer,dimension(4)::nt
@@ -13,11 +12,13 @@ real(8),dimension(10)::para
 real(8),dimension(n)::energy
 real(8),dimension(n,n)::gap
 real(8),dimension(n,8)::eigenv
+real(8),dimension(n,8)::pump_p
 real(8)::P,P1,P2,P3,Q,Q1,T,T1,R,R1
 
 para=0
-para(1)=101
+para(1)=201
 nkz=1
+gap=0
 P=1.345d0
 Q=1.139d0
 R=0.619d0
@@ -28,6 +29,8 @@ R1=1.076d0
 P3=1.424d0
 T=1.145d0
 T1=0.281d0
+
+read(*,*) laser_ev
 
 do nky=nkz,int(para(1))
     do nkx=nkz,int(para(1))
@@ -45,51 +48,61 @@ do nky=nkz,int(para(1))
                 write(13,*)  kx,ky,energy(9)
                 write(15,*)  kx,ky,energy(11)
                 write(17,*)  kx,ky,energy(13)
-                i=9
-                j=7
-                gap(i,j)=energy(i)-energy(j)
-                gap(10,10)=0.9d0-energy(j)
-                write(18,*) kx,ky,gap(i,j)
-                write(19,*) kx,ky,gap(10,10)
-                moji=abs(gap(i,j)-1.165d0)
-                if (0<=moji.and.moji<=0.01d0) then
-                    para(4)=0
-                    !jからiへの遷移を過程
-                    if (0.000<=eigenv(i,1).and.0.000<=eigenv(j,6)) then
-                    para(4)=para(4)+P**2*abs(eigenv(i,1)*eigenv(j,6))**2
-                    if (0.000<=eigenv(i,3).and.0.000<=eigenv(j,6)) then
-                    para(4)=para(4)+P1**2*abs(eigenv(i,3)*eigenv(j,6))**2  
-                    if (0.000<=eigenv(i,1).and.0.000<=eigenv(j,8)) then
-                    para(4)=para(4)+P2**2*abs(eigenv(i,3)*eigenv(j,6))**2  
-                    if (0.000<=eigenv(i,3).and.0.000<=eigenv(j,8)) then
-                    para(4)=para(4)+P3**2*abs(eigenv(i,3)*eigenv(j,6))**2  
-                    if (0.000<=eigenv(i,1).and.0.000<=eigenv(j,2)) then
-                    para(4)=para(4)+Q**2*abs(eigenv(i,3)*eigenv(j,6))**2  
-                    if (0.000<=eigenv(i,3).and.0.000<=eigenv(j,2)) then
-                    para(4)=para(4)+Q1**2*abs(eigenv(i,3)*eigenv(j,6))**2  
-                    if (0.000<=eigenv(i,1).and.0.000<=eigenv(j,7)) then
-                    para(4)=para(4)+R**2*abs(eigenv(i,3)*eigenv(j,6))**2 
-                    if (0.000<=eigenv(i,3).and.0.000<=eigenv(j,7)) then
-                    para(4)=para(4)+R1**2*abs(eigenv(i,3)*eigenv(j,6))**2 
-                    if (0.000<=eigenv(i,5).and.0.000<=eigenv(j,2)) then
-                    para(4)=para(4)+T**2*abs(eigenv(i,3)*eigenv(j,6))**2 
-                    if (0.000<=eigenv(i,4).and.0.000<=eigenv(j,2)) then
-                    para(4)=para(4)+T1**2*abs(eigenv(i,3)*eigenv(j,6))**2 
-                    end if
-                    end if
-                    end if
-                    end if
-                    end if
-                    end if
-                    end if
-                    end if
-                    end if
-                    end if
-                    write(20,*) kx,ky,gap(i,j),para(4)
 
-                else
-                 !   write(20,*) kx,ky,"nan"
-                end if
+!                全部のバンドの計算がしたい場合はdoループを解除
+  !              do i=9,30
+  !                  do j=1,8
+                        i=9
+                        j=8
+                        gap(i,j)=energy(i)-energy(j)
+                        write(18,*) kx,ky,gap(i,j)
+                        para(4)=abs(gap(i,j)-laser_ev)
+                        !write(18,*) para(4),gap(i,j)
+                        para(4)=abs(gap(i,j)-laser_ev)
+                        if (0<=para(4).and.para(4)<=0.01) then
+                            !ギャップは+-6-7nmとする,para(4)<=0.01の場合、para(1)を小さくしないとサーチにひっかからない
+                            pump_p(i,j)=1
+                            para(5)=0
+                            !jからiへの遷移を過程
+                            if (0.000<=eigenv(i,1).and.0.000<=eigenv(j,6)) then
+                            para(5)=para(5)+P*abs(eigenv(i,1)*eigenv(j,6)+eigenv(j,1)*eigenv(i,6))
+                            if (0.000<=eigenv(i,3).and.0.000<=eigenv(j,6)) then
+                            para(5)=para(5)+P1*abs(eigenv(i,3)*eigenv(j,6)+eigenv(j,3)*eigenv(i,6)) 
+                            if (0.000<=eigenv(i,1).and.0.000<=eigenv(j,8)) then
+                            para(5)=para(5)+P2*abs(eigenv(i,1)*eigenv(j,8)+eigenv(j,1)*eigenv(i,8))  
+                            if (0.000<=eigenv(i,3).and.0.000<=eigenv(j,8)) then
+                            para(5)=para(5)+P3*abs(eigenv(i,3)*eigenv(j,8)+eigenv(j,3)*eigenv(i,8))  
+                            if (0.000<=eigenv(i,1).and.0.000<=eigenv(j,2)) then
+                            para(5)=para(5)+Q*abs(eigenv(i,1)*eigenv(j,2)+eigenv(j,1)*eigenv(i,2)) 
+                            if (0.000<=eigenv(i,3).and.0.000<=eigenv(j,2)) then
+                            para(5)=para(5)+Q1*abs(eigenv(i,3)*eigenv(j,2)+eigenv(j,3)*eigenv(i,2))  
+                            if (0.000<=eigenv(i,1).and.0.000<=eigenv(j,7)) then
+                            para(5)=para(5)+R*abs(eigenv(i,1)*eigenv(j,7)+eigenv(j,1)*eigenv(i,7)) 
+                            if (0.000<=eigenv(i,3).and.0.000<=eigenv(j,7)) then
+                            para(5)=para(5)+R1*abs(eigenv(i,3)*eigenv(j,7)+eigenv(j,3)*eigenv(i,7)) 
+                            if (0.000<=eigenv(i,5).and.0.000<=eigenv(j,2)) then
+                            para(5)=para(5)+T*abs(eigenv(i,5)*eigenv(j,2)+eigenv(j,5)*eigenv(i,2)) 
+                            if (0.000<=eigenv(i,4).and.0.000<=eigenv(j,2)) then
+                            para(5)=para(5)+T1*abs(eigenv(i,4)*eigenv(j,2)+eigenv(j,4)*eigenv(i,2)) 
+                            para(5)=para(5)**2
+                            end if
+                            end if
+                            end if
+                            end if
+                            end if
+                            end if
+                            end if
+                            end if
+                            end if
+                            end if
+                            !双極子遷移モーメントのあたいはpara(5)
+                            write(211,"(3f10.4,2i10,f10.4)") laser_ev,gap(i,j),para(4),i,j,para(5)
+                            write(20,*) kx,ky,gap(i,j)
+                        else
+                            write(20,*) kx,ky,"nan"
+                        end if
+    !                end do
+   !             end do
             else
                 kx=real((nkx-1))/100
                 ky=real((nky-1))/100
